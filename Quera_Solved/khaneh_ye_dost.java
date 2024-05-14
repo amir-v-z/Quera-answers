6153bd040500323f678035b1940ab3943b615ae7
amir_v_z <amirhosseinvakilzadeh82@gmail.com>
2024-05-14 16:05:21 +0330

    first commit

---
+++ Quera_Solved/khaneh_ye_dost.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class khaneh_ye_dost {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        int n = input.nextInt();
+
+        System.out.println(n*n);
+
+        input.close();
+    }
+}
